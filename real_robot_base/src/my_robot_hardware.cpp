77e628b9c8d976d5ef48629dca3daf6250454507
Cocoloc <mini-coco@hotmail.Fr>
2021-02-17 17:32:51 +0100

    maj

---
+++ real_robot_base/src/my_robot_hardware.cpp
@@ -0,0 +1,14 @@
+#include "real_robot_base/my_robot_hardware.h"
+main()
+{
+  MyRobot robot;
+  controller_manager::ControllerManager cm(&robot);
+
+  while (true)
+  {
+     robot.read();
+     cm.update(robot.get_time(), robot.get_period());
+     robot.write();
+     sleep();
+  }
+}
